3f0449f75e949f43a32124fc1f2d22066df6af29
Sokly <hengsokly23@gmail.com>
2022-01-05 15:03:12 +0700

    remove unsued code

--- grafana/plugins/custom-plugin/src/module.ts
+++
@@ -1,6 +0,0 @@
-import { PanelPlugin } from '@grafana/data';
-import { TrackMapPanel } from './TrackMapPanel';
-import { TrackMapOptions } from './types';
-import { optionsBuilder } from './options';
-
-export const plugin = new PanelPlugin<TrackMapOptions>(TrackMapPanel).setPanelOptions(optionsBuilder);
